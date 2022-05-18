72ee89a6328c52383624ad5889bd2afe61aaf6da
wandergithub <wandergonzalez0@hotmail.com>
2022-05-18 13:13:56 -0400

    Add like, comments and reservation buttons to cards

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const displayItems = async (artistId="271256") => {
   const container = document.getElementById('section');
   const response = await fetch(`https://itunes.apple.com/lookup?id=${artistId}&entity=album&limit=6`);
   const obj = await response.json();
-  console.log(obj.results);
   for (let index = 1; index < obj.results.length; index++) {
     const element = obj.results[index];
     displayCard(container, element.artworkUrl100, element.collectionCensoredName, element.artistName);
