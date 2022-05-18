4498addf1f9930ac89ffe79aae8f961a43845d82
sunday stanley Idung <sundaystanley56@gmail.com>
2022-05-18 21:46:43 +0100

    Add comment popup details attribute

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,19 +1,16 @@
 export default class Comment {
   static createCommentPopUp = (data) => {
-    let newArticle = document.createElement('article');
-    newArticle.innerHTML = `<div> 
+    let newArticle = document.createElement('article')
+    newArticle.innerHTML = `<div class= "flex"> 
   <img src= ${data.url} alt="Image"/> 
   <span class='material-symbols-outlined'>close</span>
   </div>
   <h2>Space ${data.count}</h2>
   <div class= "pop-details">
-  <p>Fuel titatium</p>
-  <p>Length: 10000</p>
-  <p>Weight: 10000</p>
-  <p>Power: 10000</p>
+    <p>Fuel titatium</p>
+    <p>Length: 10000</p>
+    <p>Weight: 10000</p>
+    <p>Power: 10000</p>
   </div>`
-  ;
   }
-
-  static showCommentPopUp = () => {}
 }
