45add9e6ac568946052d52f8d12d0d6305065507
wandergithub <wandergonzalez0@hotmail.com>
2022-05-18 21:29:14 -0400

    Add setLike to set the items likes when user click like button

--- src/modules/likeButton.js
+++ src/modules/likeButton.js
@@ -1,3 +1,21 @@
-const manageLikes = () => {
-  
+// Manage all interactions when like icon is clicked
+async function setLike() {
+  const likeIcon = this.firstChild;
+  const likesNumber = likeIcon.dataset.likes;
+  const cardId = likeIcon.dataset.itemid;
+  const appId = JSON.parse(localStorage.getItem('appId'));
+  if (likesNumber == 0) { // If clicked first time
+    const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes/`,{
+    method:'POST',
+    body:JSON.stringify({
+      "item_id": `${cardId}`
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    console.log(response);
+  }
 }
+
+export default setLike;
