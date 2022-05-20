e15956d678f269d6deb39801191f873913a6e88f
sunday stanley Idung <sundaystanley56@gmail.com>
2022-05-20 16:47:48 +0100

    Add a new input field for the comment section

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
-import './style.css';
-import displayCard from './modules/displayCard.js';
-import Comment from './modules/comment.js';
-import initId from './modules/init.js';
-
+import './style.css'
+import displayCard from './modules/displayCard.js'
+import Comment from './modules/comment.js'
+import initId from './modules/init.js'
 // Display all items
 const displayItems = async (artistId = '271256') => {
-  const container = document.getElementById('section');
+  const container = document.getElementById('section')
   const response = await fetch(
     `https://itunes.apple.com/lookup?id=${artistId}&entity=album&limit=6`,
-  );
-  const obj = await response.json();
-  console.log(obj.results);
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }
+  )
+  const obj = await response.json()
   for (let index = 1; index < obj.results.length; index += 1) {
-    const element = obj.results[index];
+    const element = obj.results[index]
     displayCard(
       container,
       element.artworkUrl100,
       element.collectionCensoredName,
       element.artistName,
-      element.collectionId,
-    );
+      element.collectionId
+    )
   }
 
-  const btns = document.querySelectorAll('.comment');
+  const btns = document.querySelectorAll('.comment')
   Array.from(btns).forEach((btn, index) => {
-    const element = obj.results[index + 1];
-    btn.addEventListener('click', (event) => Comment.displayCommentPopUp(event, element));
+    const element = obj.results[index + 1]
+    btn.addEventListener('click', (event) =>
+      Comment.displayCommentPopUp(event, element)
+    )
+  })
+}
+displayItems()
+initId()
+
+
+document
+  .querySelector('.comment-popup')
+  .addEventListener('click', (event) => {
+    Comment.closePopUp(event);
   });
-};
-initId();
-displayItems();
-document.querySelector('.comment-popup').addEventListener('click', (event) => Comment.closePopUp(event));
+
+  const form = document.querySelector('#form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let name = document.querySelector('#input')
+    let text = document.querySelector('#textarea')
+  });
+
+
+
--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,7 +1,7 @@
-const popUp = document.querySelector('.comment-popup');
+const popUp = document.querySelector('.comment-popup')
 export default class Comment {
   static createCommentPopUp = (data) => {
-    const newArticle = document.createElement('article');
+    const newArticle = document.createElement('article')
     newArticle.innerHTML = `<div class= "flex"> 
   <img src= ${data.artworkUrl100} alt="Image"/> 
   <span class='material-symbols-outlined'>close</span>
@@ -14,32 +14,56 @@ export default class Comment {
     <p>Genre Type: ${data.primaryGenreName}</p>
   </div>
   <div class="store-comments"></div>
+  <div>
+  <h4>Add a Comment</h4>
   <form id="form">
-            <input
-              type="text"
-              name="name"
+  <input
+  type="text"
+  name="name"
               id="input"
               placeholder="Add your Name"
             />
             <br />
-            <textarea name="" id="" cols="30" rows="10" placeholder= "insights"></textarea>
+            <textarea name="" id="textarea" cols="30" rows="10" placeholder= "insights"></textarea>
             <button type="submit" class="btn">Comment</button>
-          </form>
-  `;
-    popUp.appendChild(newArticle);
+            </form>
+            </div>
+  `
+    popUp.appendChild(newArticle)
   }
 
   static displayCommentPopUp = (event, element) => {
     if (event.target.id === element.collectionId.toString()) {
-      Comment.createCommentPopUp(element);
+      Comment.createCommentPopUp(element)
     }
   }
 
   static closePopUp = (event) => {
-    const child = event.target;
-    const parentContainer = child.parentElement.parentElement.parentElement;
+    const child = event.target
+    const parentContainer = child.parentElement.parentElement.parentElement
     if (child.classList.contains('material-symbols-outlined')) {
-      parentContainer.removeChild(child.parentElement.parentElement);
+      parentContainer.removeChild(child.parentElement.parentElement)
     }
+  }
+  static postAComment = async (url, id, userName, comment) => {
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        item_id: `${id}`,
+        userName: `${userName}`,
+        comment: `${comment}`,
+      }),
+    })
+  }
+
+  static getComment = async (url) => {
+    let fetch = await fetch(url)
+    return fetch.json()
+  }
+  static getStorage = () => {
+    return JSON.parse(localStorage.getItem('appId'))
   }
 }
